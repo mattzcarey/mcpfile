fbf3c0b82680f680a75bc64b8755c588f1f06634
Matt Carey <mcarey@cloudflare.com>
2025-11-06 12:43:54 +0000

    more stuff on manager

--- packages/mcpfile/src/manager.ts
+++ packages/mcpfile/src/manager.ts
@@ -1,7 +1,12 @@
 // our client manager will wrap our client. 
-// things we want to add. we want to call manager.connectToMcpFile(filepath)
+// things we want to add. we want to call 
+// manager.connectToMcpFile(filepath)
 // manager.getState() to get the state of the manager in json
 // otherthings we need. 
-// lifecycle hooks: onConnect, onDisconnect, onMessage, onError
+// lifecycle hooks: onChange, onError
 // dump the state of this class to serializable json (for storage)
-// handle multiple servers connected. 
+// load the state of this class from serializable json (for storage)
+// handle multiple servers connected. handle them through the auth flow. 
+
+
+// i think of the getting and loading from json as simular to pydantic .to_json() and .from_json() etc.
